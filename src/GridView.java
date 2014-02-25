55f0e24dd1b644f5bd39ebc225a8e007f313b6be
RutAn <oskar_rutqvist@hotmail.com>
2014-02-25 14:14:49 +0100

    lite varnamn uppdate

--- src/GridView.java
+++ src/GridView.java
@@ -1,23 +1,31 @@
 
 import javax.swing.*;
 import java.awt.Font;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.border.BevelBorder;
 
 
 public class GridView extends JPanel {
 
 	public GridView() {
+		setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
 		setLayout(null);
 		
-		JLabel lblWarename = new JLabel("warename");
-		lblWarename.setFont(new Font("HelvLight", Font.PLAIN, 16));
-		lblWarename.setHorizontalAlignment(SwingConstants.CENTER);
-		lblWarename.setBounds(4, 90, 113, 22);
-		add(lblWarename);
+		JLabel wareNameLbl = new JLabel("warename");
+		wareNameLbl.setFont(new Font("HelvLight", Font.PLAIN, 16));
+		wareNameLbl.setHorizontalAlignment(SwingConstants.CENTER);
+		wareNameLbl.setBounds(5, 81, 113, 22);
+		add(wareNameLbl);
 		
-		JLabel lblPic = new JLabel("pic");
-		lblPic.setBounds(5, 5, 110, 110);
-		add(lblPic);
+		JLabel pictureLbl = new JLabel("pic");
+		pictureLbl.setBounds(5, 5, 110, 110);
+		add(pictureLbl);
+		
+		JLabel priceLbl = new JLabel("price");
+		priceLbl.setFont(new Font("HelvLight", Font.PLAIN, 14));
+		priceLbl.setHorizontalAlignment(SwingConstants.CENTER);
+		priceLbl.setBounds(37, 100, 46, 14);
+		add(priceLbl);
 		// TODO Auto-generated constructor stub
 	}
-
 }
