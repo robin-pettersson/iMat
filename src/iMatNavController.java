4f327e3a180b95a2642a6cb0dc311cd04562254e
unknown <robin.pettersson@fripost.org name = Robin Pettersson>
2014-02-25 14:59:59 +0100

    added backend as classpath so you can actually use it :)

--- src/iMatNavController.java
+++ src/iMatNavController.java
@@ -1,4 +1,20 @@
+import se.chalmers.ait.dat215.project.IMatDataHandler;
+
 
 public class iMatNavController {
+	
+	IMatDataHandler iMat = IMatDataHandler.getInstance();
+	
+	
+	public iMatNavController(){
 
+		
+	}
+	
+	public void navigate (String productType) {
+		
+		
+	}
+	
 }
+ 
