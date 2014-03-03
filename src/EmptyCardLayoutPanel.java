ec45ff91751c343b330509c220ded9a5c6115411
RutAn <oskar_rutqvist@hotmail.com>
2014-03-03 15:57:40 +0100

    added cardlayout panel class

---
+++ src/EmptyCardLayoutPanel.java
@@ -0,0 +1,12 @@
+
+import javax.swing.*;
+import java.awt.CardLayout;
+
+public class EmptyCardLayoutPanel extends JPanel {
+
+	public EmptyCardLayoutPanel() {
+		setLayout(new CardLayout(0, 0));
+		// TODO Auto-generated constructor stub
+	}
+
+}
