f7618cc44877fd67277165631f3f9b933900e2d8
RutAn <oskar_rutqvist@hotmail.com>
2014-03-04 13:35:25 +0100

    hopefully fixed the size of the gridview

--- src/GridView.java
+++ src/GridView.java
@@ -20,7 +20,7 @@ public class GridView extends JPanel {
 	public GridView(String name, double pris) {
 		
 		setBackground(Color.GRAY);
-		setPreferredSize(new Dimension(160, 160));
+		setPreferredSize(new Dimension(170, 170));
 		setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
 		setLayout(null);
 		
@@ -35,26 +35,26 @@ public class GridView extends JPanel {
 		priceLbl = new JLabel(pris + "kr");
 		priceLbl.setFont(new Font("HelvLight", Font.PLAIN, 14));
 		priceLbl.setHorizontalAlignment(SwingConstants.CENTER);
-		priceLbl.setBounds(5, 140, 37, 14);
+		priceLbl.setBounds(5, 150, 37, 14);
 		add(priceLbl);
 		
 		amountSpinner = new JSpinner();
 		amountSpinner.setModel(new SpinnerNumberModel(new Integer(1), new Integer(1), null, new Integer(1)));
 		amountSpinner.setName("amountSpinner");
 		amountSpinner.setFont(new Font("HelvLight", Font.PLAIN, 14));
-		amountSpinner.setBounds(47, 135, 37, 20);
+		amountSpinner.setBounds(47, 145, 37, 20);
 		add(amountSpinner);
 		
 		buyBtn = new JButton("Add");
 		buyBtn.addMouseListener(reg);
 		buyBtn.setName("buyBtn");
 		buyBtn.setFont(new Font("HelvLight", Font.PLAIN, 12));
-		buyBtn.setBounds(87, 135, 69, 20);
+		buyBtn.setBounds(92, 145, 74, 20);
 		add(buyBtn);
 		
 		pictureLbl = new JLabel("pic");
 		pictureLbl.setBackground(Color.PINK);
-		pictureLbl.setBounds(5, 5, 150, 150);
+		pictureLbl.setBounds(5, 5, 160, 160);
 		add(pictureLbl);
 		
 	}
