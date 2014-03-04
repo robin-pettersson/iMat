581802c6acb0aaad2697b00efd6bdc4cdc37a79b
RutAn <oskar_rutqvist@hotmail.com>
2014-03-04 16:09:24 +0100

    finally cartview fixed

--- src/CartView.java
+++ src/CartView.java
@@ -8,17 +8,20 @@ import javax.swing.border.LineBorder;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.border.BevelBorder;
 
 public class CartView extends JPanel {
 
 	public CartView() {
+		setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
 		setPreferredSize(new Dimension(635, 550));
 		setBackground(Color.LIGHT_GRAY);
 		setLayout(null);
 		
 		JLabel cartBreadcrumb = new JLabel("Cart");
 		cartBreadcrumb.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-		cartBreadcrumb.setBounds(10, 38, 35, 20);
+		cartBreadcrumb.setBounds(10, 40, 35, 20);
 		cartBreadcrumb.setToolTipText("You are here!");
 		cartBreadcrumb.setForeground(Color.DARK_GRAY);
 		cartBreadcrumb.setFont(new Font("HelvLight", Font.BOLD, 14));
@@ -28,7 +31,7 @@ public class CartView extends JPanel {
 		headerPanel.setLayout(null);
 		headerPanel.setBorder(new LineBorder(new Color(0, 0, 0)));
 		headerPanel.setBackground(Color.LIGHT_GRAY);
-		headerPanel.setBounds(0, 0, 635, 37);
+		headerPanel.setBounds(3, 3, 629, 37);
 		add(headerPanel);
 		
 		JLabel cartLabel = new JLabel("Cart");
@@ -41,79 +44,68 @@ public class CartView extends JPanel {
 		container.setLayout(null);
 		container.setBorder(new LineBorder(new Color(0, 0, 0)));
 		container.setBackground(Color.LIGHT_GRAY);
-		container.setBounds(0, 61, 635, 429);
+		container.setBounds(3, 61, 629, 408);
 		add(container);
 		
 		JPanel pyramidPanel = new JPanel();
 		pyramidPanel.setBorder(new LineBorder(new Color(0, 0, 0)));
-		pyramidPanel.setBounds(0, 491, 635, 29);
+		pyramidPanel.setBounds(3, 471, 629, 29);
 		add(pyramidPanel);
 		pyramidPanel.setPreferredSize(new Dimension(635, 35));
 		pyramidPanel.setBackground(Color.LIGHT_GRAY);
-		GridBagLayout gbl_pyramidPanel = new GridBagLayout();
-		gbl_pyramidPanel.columnWidths = new int[]{28, 28, 31, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_pyramidPanel.rowHeights = new int[]{26, 0};
-		gbl_pyramidPanel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_pyramidPanel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
-		pyramidPanel.setLayout(gbl_pyramidPanel);
+		pyramidPanel.setLayout(null);
 		
 		JLabel label = new JLabel("<<");
+		label.setBounds(11, 1, 28, 25);
 		label.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		label.setVerticalAlignment(SwingConstants.BOTTOM);
 		label.setToolTipText("Previous");
 		label.setHorizontalAlignment(SwingConstants.LEFT);
 		label.setForeground(Color.GRAY);
 		label.setFont(new Font("HelvLight", Font.BOLD, 20));
-		GridBagConstraints gbc_label = new GridBagConstraints();
-		gbc_label.anchor = GridBagConstraints.WEST;
-		gbc_label.insets = new Insets(0, 0, 0, 5);
-		gbc_label.gridx = 1;
-		gbc_label.gridy = 0;
-		pyramidPanel.add(label, gbc_label);
+		pyramidPanel.add(label);
 		
 		JLabel label_1 = new JLabel("1/2");
+		label_1.setBounds(315, 1, 31, 25);
 		label_1.setVerticalAlignment(SwingConstants.BOTTOM);
 		label_1.setToolTipText("Sida 1 av 2");
 		label_1.setHorizontalAlignment(SwingConstants.CENTER);
 		label_1.setForeground(Color.GRAY);
 		label_1.setFont(new Font("HelvLight", Font.BOLD, 20));
-		GridBagConstraints gbc_label_1 = new GridBagConstraints();
-		gbc_label_1.anchor = GridBagConstraints.WEST;
-		gbc_label_1.insets = new Insets(0, 0, 0, 5);
-		gbc_label_1.gridx = 11;
-		gbc_label_1.gridy = 0;
-		pyramidPanel.add(label_1, gbc_label_1);
+		pyramidPanel.add(label_1);
 		
 		JLabel label_2 = new JLabel(">>");
+		label_2.setBounds(591, 1, 28, 25);
 		label_2.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		label_2.setVerticalAlignment(SwingConstants.BOTTOM);
 		label_2.setToolTipText("Next");
 		label_2.setHorizontalAlignment(SwingConstants.RIGHT);
 		label_2.setForeground(Color.DARK_GRAY);
 		label_2.setFont(new Font("HelvLight", Font.BOLD, 20));
-		GridBagConstraints gbc_label_2 = new GridBagConstraints();
-		gbc_label_2.anchor = GridBagConstraints.WEST;
-		gbc_label_2.insets = new Insets(0, 0, 0, 5);
-		gbc_label_2.gridx = 20;
-		gbc_label_2.gridy = 0;
-		pyramidPanel.add(label_2, gbc_label_2);
+		pyramidPanel.add(label_2);
 		
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setBorder(new LineBorder(new Color(0, 0, 0)));
 		buttonPanel.setBackground(Color.LIGHT_GRAY);
-		buttonPanel.setBounds(0, 521, 635, 29);
+		buttonPanel.setBounds(3, 502, 629, 34);
 		add(buttonPanel);
 		buttonPanel.setLayout(null);
 		
-		JButton btnCheckOut = new JButton("Check out");
-		btnCheckOut.setFont(new Font("HelvLight", Font.PLAIN, 12));
-		btnCheckOut.setBounds(510, 3, 115, 23);
-		buttonPanel.add(btnCheckOut);
-		
 		JButton btnKeepShopping = new JButton("Keep shopping");
-		btnKeepShopping.setFont(new Font("HelvLight", Font.PLAIN, 12));
-		btnKeepShopping.setBounds(10, 3, 115, 23);
+		btnKeepShopping.setToolTipText("Cancel purchase");
+		btnKeepShopping.setForeground(Color.DARK_GRAY);
+		btnKeepShopping.setFont(new Font("HelvLight", Font.PLAIN, 14));
+		btnKeepShopping.setBackground(Color.WHITE);
+		btnKeepShopping.setBounds(10, 4, 142, 27);
 		buttonPanel.add(btnKeepShopping);
+		
+		JButton btnCheckOut = new JButton("Check out");
+		btnCheckOut.setToolTipText("Cancel purchase");
+		btnCheckOut.setForeground(Color.DARK_GRAY);
+		btnCheckOut.setFont(new Font("HelvLight", Font.PLAIN, 14));
+		btnCheckOut.setBackground(Color.WHITE);
+		btnCheckOut.setBounds(513, 4, 106, 27);
+		buttonPanel.add(btnCheckOut);
 		// TODO Auto-generated constructor stub
 	}
 }
