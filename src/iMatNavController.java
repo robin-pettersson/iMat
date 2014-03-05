d087c20ae2f55e09419743906a8ea13c26e822b8
algach <albinganslandt@hotmail.com>
2014-03-05 16:03:34 +0100

    Navigation almost don

--- src/iMatNavController.java
+++ src/iMatNavController.java
@@ -1,5 +1,6 @@
 import java.awt.CardLayout;
 import java.awt.Component;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JPanel;
@@ -13,61 +14,114 @@ import javax.swing.tree.DefaultMutableTreeNode;
 
 
 
+
+import javax.swing.tree.MutableTreeNode;
+
+import com.sun.org.apache.xalan.internal.lib.NodeInfo;
+
 import se.chalmers.ait.dat215.project.IMatDataHandler;
 import se.chalmers.ait.dat215.project.Product;
 import se.chalmers.ait.dat215.project.ProductCategory;
 
 
 public class iMatNavController implements TreeSelectionListener {
-	
+
 	IMatDataHandler iMat = IMatDataHandler.getInstance();
+	private DefaultMutableTreeNode node;
 	private static iMatNavController navController = null;
-	
-	
-	
+
+	private int currentPage = 0;
+	private int currentIndex = 0;
+	private ProductCategory[] categorys;
+	private JPanel wareContainer;
+	private List<Product> products;
+
 	public iMatNavController(){
-		
-		
+		products = new ArrayList<>();
 	}
-	
+
 	public static iMatNavController getInstance(){
 		if(navController != null)
 			return navController;
 		return new iMatNavController();
 	}
-	
-	public void navigate (String productType) {
-		
-		
-	}
-	
+
 	@Override
 	public void valueChanged(TreeSelectionEvent e) {
+
 		JTree navigationTree = (JTree) e.getSource();
-		DefaultMutableTreeNode node = (DefaultMutableTreeNode)
-				navigationTree.getLastSelectedPathComponent();
-
-		if (node == null) return;
-		
+		node = (DefaultMutableTreeNode)navigationTree.getLastSelectedPathComponent();
 		Object nodeInfo = node.getUserObject();
-		String type = nodeInfo.toString().replace(' ', '_');
-		type = type.toUpperCase();
-		System.out.print(type);
-		JPanel comp = (JPanel) navigationTree.getParent().getParent().getComponent(1);
-		CardLayout layout = (CardLayout) comp.getLayout();
-		System.out.println(comp);
-		layout.first(comp);
-		layout.next(comp);
-		
-		JPanel p = (JPanel) comp.getComponent(1);
-		JPanel panel = (JPanel) p.getComponent(1);
 
-		listItems(type, panel);
+		String type = toCategoryString(nodeInfo.toString());
+		System.out.print(type);
+
+		JPanel parent = (JPanel) navigationTree.getParent().getParent().getComponent(1);
+		CardLayout layout = (CardLayout) parent.getLayout();
+		wareContainer = (JPanel) ((JPanel) parent.getComponent(1)).getComponent(1);
+		layout.first(parent);
+		layout.next(parent);
+
+		setCategorys(type);
+		listItems();
 	}
-	
-	public void listItems(String type, JPanel panel){
-		panel.removeAll();
+
+
+	public void listItems(){
+		wareContainer.removeAll();
+		for (int i = (12*currentPage) ; i < (12*currentPage + 12) && i < products.size() ; i++) {
+			Product pro = products.get(i);
+			GridView card = new GridView(pro.getName(), pro.getPrice());
+			wareContainer.add(card);
+			wareContainer.revalidate();
+		}
+	}
+
+	private  void setCategorys(String type) {
+		ProductCategory[] cats = new ProductCategory[20];
+		DefaultMutableTreeNode currentNode = node;
+		DefaultMutableTreeNode lastNode = null;
+		boolean done = true;
+		int lastIndex = 0;
+		int size = 0;
+
+		if(node.isLeaf()){
+			cats[0] = getCategory(type);
+			size++;
+			combineList(cats);
+		}
+
+
+		for(int i = 0; i < currentNode.getChildCount(); i++){
+			DefaultMutableTreeNode subNode = (DefaultMutableTreeNode) currentNode.getChildAt(i);
+
+			System.out.println(subNode.isLeaf());
+			if(subNode.isLeaf()){
+				System.out.println("Adding Category: " + getCategory(subNode.toString()));
+				cats[size] = getCategory(subNode.toString());
+				size++;
+			}
+			else{
+				System.out.println("has a subtree");
+				lastIndex = i;
+				lastNode = currentNode;
+				i = -1;
+				currentNode = subNode;
+				done = false;
+			}
+			if(!done && i + 1 == currentNode.getChildCount()){
+				System.out.println("back to parent");
+				i = lastIndex;
+				currentNode = lastNode;
+				done = true;
+			}
+		}
+		combineList(cats);
+	}
+
+	private ProductCategory getCategory(String type){
 		ProductCategory category = null;
+		type = toCategoryString(type);
 		for (ProductCategory cat : ProductCategory.values()) {
 			String name = cat.name();
 			if(name.equals(type)){
@@ -75,15 +129,19 @@ public class iMatNavController implements TreeSelectionListener {
 				break;
 			}
 		}
-		
-		List<Product> productList = iMat.getProducts(category);
-		for (int i = 0; i < 12 || i < iMat.getProducts(category).size() ; i++) {
-			Product pro = productList.get(i);
-			GridView card = new GridView(pro.getName(), pro.getPrice());
-			panel.add(card);
-			panel.revalidate();
+		return category;
+	}
+
+	private void combineList(ProductCategory[] cats) {
+		for(ProductCategory cat : cats){
+			for(Product prod : iMat.getProducts(cat)){
+				products.add(prod);
+			}
 		}
-		
+	}
+
+	private String toCategoryString(String s) {
+		return s.replace(' ', '_').toUpperCase();
 	}
 
 }
